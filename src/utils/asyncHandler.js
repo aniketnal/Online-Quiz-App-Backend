6741845b871a219875bc4d2bfbe927bfb494f356
aniketnal <aniketnalegaonkar44@gmail.com>
2025-10-04 00:53:06 +0530

    added app and index

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+// A higher order function that handles async functions gracefully
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise
+        .resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
