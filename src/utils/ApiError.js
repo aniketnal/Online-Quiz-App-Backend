0330ba725e2d2f59374ac4ca9ff3b82e985966fa
aniketnal <aniketnalegaonkar44@gmail.com>
2025-10-04 17:01:30 +0530

    fixed minor typos

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -13,9 +13,9 @@ class ApiError extends Error {
         this.errors = errors
         
         if(stack) {
-            this.statck = stack
+            this.stack = stack
         }else {
-            Error.captureStackTrace(this, this.constrcutor)
+            Error.captureStackTrace(this, this.constructor)
         }
 
     }
